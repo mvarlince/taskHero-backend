dce8dbdf448ee4f3fd2c9ae89a2088598d62c872
Varlince Meran <varlincemeran@gmail.com>
2023-02-15 11:42:28 -0500

    added addTask and updateTask functions

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,14 @@
 import functions from "firebase-functions"
 import express from "express"
 import cors from "cors"
-import { getAllTasks } from "./src/tasks.js"
+import { addTask, getAllTasks, updateTask } from "./src/tasks.js"
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 app.get('/tasks', getAllTasks)
-// app.post('/tasks', )
-// app.patch('tasks/:taskId')
+app.post('/tasks', addTask)
+app.patch('tasks/:taskId', updateTask)
 
 export const api = functions.https.onRequest(app)
-
--- functions/src/tasks.js
+++ functions/src/tasks.js
@@ -1,15 +1,39 @@
 import getFirestoreInstance from "./utils.js";
+import { FieldValue } from "firebase-admin/firestore";
 
 
 export function getAllTasks(req, res){
     console.log("--- hitting function-----")
     const db = getFirestoreInstance()
-    console.log("-- connected to firestore ---")
-    db.collection('tasks').get()
+    console.log("--- connected to db ---")
+    db.collection('tasks')
+    .sort('createdAt', 'desc')
+    .get()
     .then(collection => {
-        console.log("-- getting collection ---")
+        console.log("--- getting collection ---")
         const tasks = collection.docs.map(doc => ({taskId: doc.id, ...doc.data()}))
         res.send(tasks)
     })
     .catch(err => res.status(500).json({error: err.message}))
+}
+
+export async function addTask(req,res) {
+    const {task} = req.body
+    const newTask = {task, createdAt: FieldValue.serverTimestamp()}
+    const db = getFirestoreInstance()
+    db.collection('tasks')
+    .add(newTask)
+    .then(res.send( () => getAllTasks(req,res) ))
+    .catch(err => res.status(500).send({error: err.message}))
+}
+
+export async function updateTask(req, res) {
+    const { done } = req.body
+    const { taskId } = req.params
+    const db = getFirestoreInstance()
+    db.collection('tasks')
+    .doc(taskId)
+    .update({done})
+    .then( () => getAllTasks(req,res) )
+    .catch(err => res.status(500).send({error: err.message}))
 }
